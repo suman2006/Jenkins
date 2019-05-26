b2c2937c7bfd7d7398ce479565fce1e6b53281bc
Automasters12 <suman143learning@gmail.com>
2019-05-26 11:06:47 +0530

    updating project

---
+++ BBB/src/test/java/tests/TestSuite2.java
@@ -0,0 +1,24 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestSuite2 {
+
+	@Test
+	public void test1() {
+		System.out.println("test");
+	}
+	@Test
+	public void test2() {
+		System.out.println("test");
+	}
+
+	@Test
+	public void test3() {
+		System.out.println("test");
+	}
+
+
+}
